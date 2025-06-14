8bca8b915367c5cd11520fc2469883faa5edc9e2
Bhautik <bhirpara600@gmail.com>
2025-06-14 13:54:50 +0530

    Render multiple card

--- Part-02/01_Project-01(Country_API)/src/Components/CountryCard.jsx
+++ Part-02/01_Project-01(Country_API)/src/Components/CountryCard.jsx
@@ -1,13 +1,13 @@
-function CountryCard() 
+function CountryCard({ countryName, population, imgURL, region, capital }) 
 {
     return (
-        <a className="country-card" href="/country.html?name=Togo">
-            <img src="https://flagcdn.com/tg.svg" alt="Togo flag" />
+        <a className="country-card" href="/">
+            <img src={imgURL} alt={`${countryName} Flag`} />
             <div className="card-text">
-                <h3 className="card-title">Togo</h3>
-                <p><b>Population: </b>82,78,737 </p>
-                <p><b>Region: </b>Africa</p>
-                <p><b>Capital: </b>Lomé</p>
+                <h3 className="card-title">{countryName}</h3>
+                <p><b>Population: </b>{population}</p>
+                <p><b>Region: </b>{region}</p>
+                <p><b>Capital: </b>{capital}</p>
             </div>
         </a>
     )
--- Part-02/01_Project-01(Country_API)/src/Components/CountryList.jsx
+++ Part-02/01_Project-01(Country_API)/src/Components/CountryList.jsx
@@ -1,9 +1,19 @@
 import CountryCard from "./CountryCard";
+import countryData from "./countriesData"
 
 function CountryList() {
     return (
         <div className="countries-container">
-            <CountryCard />
+            {
+                countryData.map((country) => <CountryCard 
+                    key={country.name.common}
+                    countryName={country.name.common}
+                    population={country.population.toLocaleString("en-IN")}
+                    imgURL={country.flags.svg}
+                    region={country.region}
+                    capital={country.capital?.[0]}
+                />)
+            }
         </div>
     )
 }
