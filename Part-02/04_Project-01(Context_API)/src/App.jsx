7703be739a441d4d67cc26aabb44a1b69738a9eb
Bhautik <bhirpara600@gmail.com>
2025-06-17 16:13:23 +0530

    Fix bug

--- Part-02/04_Project-01(Context_API)/src/App.jsx
+++ Part-02/04_Project-01(Context_API)/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Components/Header"
 import { Outlet } from "react-router"
-import { ThemeProvider } from "./Contexts/themeContext"
+import { ThemeProvider } from "./Contexts/ThemeContext"
 import { SearchQueryProvider } from "./Contexts/SearchQueryContext"
 
 export default function App() {
--- Part-02/04_Project-01(Context_API)/src/Hooks/useTheme.js
+++ Part-02/04_Project-01(Context_API)/src/Hooks/useTheme.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../Contexts/themeContext";
+import { ThemeContext } from "../Contexts/ThemeContext";
 
 export function useTheme() {
     return useContext(ThemeContext)
