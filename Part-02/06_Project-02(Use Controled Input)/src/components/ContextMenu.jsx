b8c9d60bd648db33254c54e160de2d54f7f18ff4
Bhautik <bhirpara600@gmail.com>
2025-06-19 23:02:01 +0530

    Add context menu & delete functionality

--- Part-02/06_Project-02(Use Controled Input)/src/components/ContextMenu.jsx
+++ Part-02/06_Project-02(Use Controled Input)/src/components/ContextMenu.jsx
@@ -1,8 +1,17 @@
-export default function ContextMenu() {
+export default function ContextMenu({menuPosition, setMenuPosition, setExpenses, rowId}) {
+    if(!menuPosition.left || !menuPosition.top) return
+
     return (
-        <div className="context-menu">
-            <div>Edit</div>
-            <div>Delete</div>
+        <div className="context-menu" style={{...menuPosition}}>
+            <div onClick={() => {
+                console.log("Editing");
+                setMenuPosition({})
+            }}>Edit</div>
+
+            <div onClick={() => {
+                setExpenses((prevState) => prevState.filter(expense => expense.id !== rowId))
+                setMenuPosition({})
+            }}>Delete</div>
         </div>
     )
 }
