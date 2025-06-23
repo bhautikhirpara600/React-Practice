b0177f9f3785f846b1b12375ba4bb0d59f6f4bba
Bhautik <bhirpara600@gmail.com>
2025-06-23 16:53:33 +0530

    Use Modal Component in Contact page

--- Part-02/07_Project-03(Popup)/src/components/Contact.jsx
+++ Part-02/07_Project-03(Popup)/src/components/Contact.jsx
@@ -1,5 +1,31 @@
+import { usePopup } from "../hooks/usePopup";
+import Modal from "./Modal";
+
 export default function Contact() {
+
+    const [displayPopup, setDisplayPopup] = usePopup()
+
     return (
-        <h1 className="text-2xl text-white bg-cyan-500 shadow-lg shadow-cyan-500/50 rounded-md  max-w-max m-auto font-bold text-center p-2 mt-4" >This is a Contact Page.</h1>
+        <>
+            <h1 className="text-2xl text-white bg-cyan-500 shadow-lg shadow-cyan-500/50 rounded-md  max-w-max m-auto font-bold text-center p-2 mt-4" >This is a Contact Page.</h1>
+            <div className="flex justify-center mt-4">
+                <button onClick={() => setDisplayPopup(true)} className='bg-cyan-500 px-4 py-2 text-white rounded-sm font-semibold cursor-pointer'>Show</button>
+            </div>
+            <Modal
+                header={<div className="font-bold text-2xl">Header</div>}
+                footer={
+                    <div className="flex justify-end">
+                        <button onClick={() => setDisplayPopup(false)} className='bg-cyan-500 px-4 py-2 text-white rounded-sm font-semibold cursor-pointer'>Close</button>
+                    </div>
+                }
+            >
+                <div className="my-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur minus harum distinctio temporibus quas praesentium fugit dolores
+                    dignissimos corrupti autem vitae, adipisci, exercitationem dolore
+                    laudantium quasi ipsum! Voluptate,quidem at!
+                </div>
+            </Modal>
+        </>
     )
 }
