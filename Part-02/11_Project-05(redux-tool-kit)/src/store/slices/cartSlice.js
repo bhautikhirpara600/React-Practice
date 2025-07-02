f31dc91741cb83880f8be77ef225630ddc52db9b
Bhautik <bhirpara600@gmail.com>
2025-07-02 13:31:09 +0530

    Use Redux-Toolkit

--- Part-02/11_Project-05(redux-tool-kit)/src/store/slices/cartSlice.js
+++ Part-02/11_Project-05(redux-tool-kit)/src/store/slices/cartSlice.js
@@ -1,44 +1,39 @@
-import { produce } from "immer"
+import { createSlice } from "@reduxjs/toolkit"
 
-// Action types
-export const CART_ADD_PRODUCT = "cart/addProduct"
-export const CART_REMOVE_PRODUCT = "cart/removeProduct"
-export const CART_INCREASE_ITEM_QUANTITY = "cart/increaseItemQuantity"
-export const CART_DECREASE_ITEM_QUANTITY = "cart/decreaseItemQuantity"
+const findItemIndex = (state, action) => state.findIndex(cartItem => cartItem.productId === action.payload.productId)
 
-// Action creators
-export const addCartProduct = (productData) => ({ type: CART_ADD_PRODUCT, payload: productData })
-export const removeCartProduct = (productId) => ({ type: CART_REMOVE_PRODUCT, payload: { productId } })
-export const increaseCartItemQuantity = (productId) => ({ type: CART_INCREASE_ITEM_QUANTITY, payload: { productId } })
-export const decreaseCartItemQuantity = (productId) => ({ type: CART_DECREASE_ITEM_QUANTITY, payload: { productId } })
-
-// reducer
-export default function cartReducer(originalState = [], action) {
-    return produce(originalState, (state) => {
-        const existingItemIndex = state.findIndex(cartItem => cartItem.productId === action.payload.productId)
-        switch(action.type) {
-        case CART_ADD_PRODUCT:
+const slice = createSlice({
+    name: "cart",
+    initialState: [],
+    reducers: {
+        addCartProduct(state, action) {
+            const existingItemIndex = findItemIndex(state, action)
             if (existingItemIndex !== -1) {
                 state[existingItemIndex].quantity += 1
-                return state
             }else {
                 state.push({...action.payload, quantity: 1})
-                return state
             }
-        case CART_REMOVE_PRODUCT:
+        },
+        removeCartProduct(state, action) {
+            const existingItemIndex = findItemIndex(state, action)
             state.splice(existingItemIndex, 1)
-            return state
-        case CART_INCREASE_ITEM_QUANTITY:
+        },
+        increaseCartItemQuantity(state, action) {
+            const existingItemIndex = findItemIndex(state, action)
             state[existingItemIndex].quantity += 1
-            return state
-        case CART_DECREASE_ITEM_QUANTITY:
+        },
+        decreaseCartItemQuantity(state, action) {
+            const existingItemIndex = findItemIndex(state, action)
             state[existingItemIndex].quantity -= 1
             if(state[existingItemIndex].quantity === 0) {
                 state.splice(existingItemIndex, 1)
             }
-            return state
-        default:
-            return state
+        }
     }
-    })
-}
+})
+
+// console.dir(slice);
+
+export default slice.reducer
+
+export const { addCartProduct, removeCartProduct, increaseCartItemQuantity, decreaseCartItemQuantity } = slice.actions
--- Part-02/11_Project-05(redux-tool-kit)/src/store/store.js
+++ Part-02/11_Project-05(redux-tool-kit)/src/store/store.js
@@ -1,12 +1,13 @@
-import { combineReducers, createStore } from "redux"
 import productReducer from "./slices/productSlice"
 import cartReducer from "./slices/cartSlice"
 import wishListReducer from "./slices/wishListSlice"
+import { configureStore } from "@reduxjs/toolkit"
 
-const reducer = combineReducers({
+
+export const store = configureStore({
+    reducer: {
     products: productReducer,
     cart: cartReducer,
     wishList: wishListReducer
+}
 })
-
-export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
