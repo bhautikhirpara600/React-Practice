ef1310b5e2d5ef84abd97e3e16f04436d5ed14bf
Bhautik <bhirpara600@gmail.com>
2025-07-23 12:01:40 +0530

    Use middleware blueprint

--- Part-02/12_Project-05(middle-ware)/src/store/store.js
+++ Part-02/12_Project-05(middle-ware)/src/store/store.js
@@ -2,6 +2,7 @@ import productReducer from "./slices/productSlice"
 import cartReducer from "./slices/cartSlice"
 import wishListReducer from "./slices/wishListSlice"
 import { configureStore } from "@reduxjs/toolkit"
+import { logger } from "./middleware/logger"
 
 
 export const store = configureStore({
@@ -9,5 +10,6 @@ export const store = configureStore({
     products: productReducer,
     cart: cartReducer,
     wishList: wishListReducer
-}
+},
+middleware: (defaultMiddleware) => [...defaultMiddleware()]
 })
